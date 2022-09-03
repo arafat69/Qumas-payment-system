f81c6c4fdb3c77c56c9683c2cf983e2c5c0c9744
arafat69 <arafataliar69@gmail.com>
2022-09-03 16:17:23 +0600

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+/*!
+    * Start Bootstrap - SB Admin v7.0.5 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2022 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+    */
+    // 
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+
+
+$(document).ready(function(){
+
+    $messageBoxShow = document.querySelectorAll('#messageBoxShow');
+    $messageBox = document.querySelectorAll('#message-box');
+
+    //hide message box
+    $("#message-box-close-icon").click(function(){
+        $($messageBox).hide();
+      });
+
+      //show message box
+      $($messageBoxShow).click(function(){
+        $($messageBox).show();
+      });
+
+})
